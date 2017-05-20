1bfcc2649ab821be3274da91b0b3808176f66256
Nicolas Homble <nhomble@terpmail.umd.edu>
2017-05-20 08:51:14 -0400

    not on my netbook

--- config.h
+++ config.h
@@ -29,8 +29,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "NOTES",    NULL,		  NULL,		  0,			True,		 -1 },
 };
 
