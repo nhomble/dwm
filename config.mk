98dd3b77bea3cb1df3f1df532725b6b776ce17e4
nicolas <nhomble@terpmail.umd.edu>
2019-12-29 01:29:20 -0500

    nvim

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ VERSION = 6.2
 # Customize below to fit your system
 TODO = /home/${SUDO_USER}/.TODO
 BIN = $$(pwd)/bin/
-EDITOR = $$(which vim)
+EDITOR = $$(which nvim)
 
 # paths
 PREFIX = /usr/local
